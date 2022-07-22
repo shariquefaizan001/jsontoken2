8301aba1ccd4f5ae162467343e1846f571200e43
sharique001 <sharique.immanent@gmail.com>
2022-07-22 15:06:01 +0530

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
   }, [])
   return (
     <>
-      <div>Welcome  to  home page  all of you </div>
+      <div>my page</div>
       <div>
         {
           state && JSON.stringify(state)}
